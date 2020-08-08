b5a7261764ac02630f03ded453f5a8b99fccd6fd
ajose4dk <ajose4dk@users.noreply.github.com>
2020-08-08 16:35:42 -0400

    Create BitmapWriter.h

---
+++ include/common/bit/BitmapWriter.h
@@ -0,0 +1,66 @@
+#ifndef __MSERF_BIT_UTIL_BITMAP_WRITER_H__
+#define __MSERF_BIT_UTIL_BITMAP_WRITER_H__
+
+//CREDIT: taken from arrow-master/cpp/src/arrow/util
+//
+#include <cstdint>
+#include <cstring>
+
+#include "common/bit/BitUtil.h"
+
+namespace Msef {
+
+namespace BitUtil {
+
+class BitmapWriter {
+  // A sequential bitwise writer that preserves surrounding bit values.
+
+ public:
+  BitmapWriter(uint8_t* bitmap, int64_t start_offset, int64_t length)
+      : bitmap_(bitmap), position_(start_offset), length_(length) {
+    byte_offset_ = start_offset / 8;
+    bit_mask_ = kBitmask[start_offset % 8];
+    if (length > 0) {
+      current_byte_ = bitmap[byte_offset_];
+    } else {
+      current_byte_ = 0;
+    }
+  }
+
+  void Set() { current_byte_ |= bit_mask_; }
+
+  void Clear() { current_byte_ &= bit_mask_ ^ 0xFF; }
+
+  void Next() {
+    bit_mask_ = static_cast<uint8_t>(bit_mask_ << 1);
+    ++position_;
+    if (bit_mask_ == 0) {
+      // Finished this byte, need advancing
+      bit_mask_ = 0x01;
+      bitmap_[byte_offset_++] = current_byte_;
+      if (TP_PREDICT_TRUE(position_ < length_)) {
+        current_byte_ = bitmap_[byte_offset_];
+      }
+    }
+  }
+
+  void Finish() {
+    // Store current byte if we didn't went past bitmap storage
+    if (length_ > 0 && (bit_mask_ != 0x01 || position_ < length_)) {
+      bitmap_[byte_offset_] = current_byte_;
+    }
+  }
+
+  int64_t position() const { return position_; }
+
+ private:
+  uint8_t* bitmap_;
+  int64_t position_;
+  int64_t length_;
+
+  uint8_t current_byte_;
+  uint8_t bit_mask_;
+  int64_t byte_offset_;
+};
+
+}   // END OF NS: BitUtil
